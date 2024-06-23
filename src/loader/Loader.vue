f32526c500281b8c8544d55526021757f013f1b9
Theodora Konstantinou <theodor.conn@gmail.com>
2024-06-24 02:15:04 +0300

    Add loader

---
+++ src/loader/Loader.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container class="loader-container" fill-height>
+    <v-row class="loader-row">
+      <v-col cols="auto">
+        <v-img
+          class="rotating-image"
+          :src="pokeball"
+          alt="Loading..."
+          width="100"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+import pokeball from '../assets/pokeball.png';
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/styles.scss';
+
+.loader-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60vh;
+}
+.loader-row {
+  height: 100%;
+}
+
+.rotating-image {
+  animation: rotate 2s linear infinite;
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
